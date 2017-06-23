7eb3c48432f9c97421f6b29f4aa3bbd1ec0f0998
Garima Jain <garimaj2108@gmail.com>
2017-06-23 16:06:42 -0700

    Update method to check elements belonging to a column

--- sudoku.rb
+++ sudoku.rb
@@ -30,4 +30,21 @@ end
  	string.split('').map(&:to_i)
  end
 
+# Identifies the cells in each column and returns elements of each column
+def column_identify(array, column_number)
+  columns = []
+  array.each_index do |cell|
+    if column_number == cell % 9
+      columns << array[cell]
+    end
+  end
+  columns
+end
+
+# Driver code to test column_identify method
+#my_array = convert_to_array("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
+
+#p column_identify(my_array,8)
+
+
 
